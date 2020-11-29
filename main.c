f92b4c2cdfcc8a6b6a905328ec68548fa62105e1
farah <farah.woelfl@gmail.com>
2020-11-29 19:49:49 +0100

    programm with function SQRT without math.h lib

--- main.c
+++ main.c
@@ -1,13 +1,72 @@
 
 #include <stdio.h>
-#include <math.h>
+float findSQRT(int number)
+{
+    int start = 0, end = number;
+    int mid;
+
+    // To store the answer
+    float ans;
+
+    // To find integral part of square
+    // root of number
+    while (start <= end)
+    {
+
+        // Find mid
+        mid = (start + end) / 2;
+
+        // If number is perfect square
+        // then break
+        if (mid * mid == number)
+        {
+            ans = mid;
+            break;
+        }
+
+        // Increment start if integral
+        // part lies on right side
+        // of the mid
+        if (mid * mid < number)
+        {
+            start = mid + 1;
+            ans = mid;
+        }
+
+            // Decrement end if integral part
+            // lies on the left side of the mid
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    // To find the fractional part
+    // of square root upto 5 decimal
+    float increment = 0.1;
+    for(int i = 0; i < 5; i++)
+    {
+        while (ans * ans <= number)
+        {
+            ans += increment;
+        }
+
+        // Loop terminates,
+        // when ans * ans > number
+        ans = ans - increment;
+        increment = increment / 10;
+    }
+    return ans;
+}
+
+// Driver Code
 
 int main() {
     int counter = 0;
     int n;
     printf(": ");
     scanf("%d", &n);
-    int sqrtN = (int) sqrt(n);
+    int sqrtN = (int) findSQRT(n);
     int isNotPrim[n+1];
     for (int i = 2; i < n+1; i++){
         isNotPrim[i] = 1;               // initialize all entries with false
